586dac8a1c950db199f1b0dcb1e90fb9889c7925
bormand <bormandt@yandex.ru>
2018-09-12 22:58:28 +0700

    Various api changes (search, users, ignores, cloud flare fix)

--- webapp/api.py
+++ webapp/api.py
@@ -3,6 +3,8 @@ import flask
 import logging
 from datetime import datetime, timezone, timedelta
 from schema import ScopedSession, SyncState, User, Post, Comment
+from sqlalchemy.sql.expression import func
+import re
 
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
@@ -23,20 +25,43 @@ def state():
         "total": total
     })
 
+def normalize_text(s):
+    # TODO: hacks below should be integrated into the parser
+    s = s.replace("&#13;", "")
+
+    res = ""
+    while True:
+        m = re.search(r'<a.*?data-cfemail="(.*?)">.*?</a>', s)
+        if not m:
+            break
+
+        encoded = bytes.fromhex(m.group(1))
+        key = encoded[0]
+        decoded = "".join([chr(c ^ key) for c in encoded[1:]])
+
+        res += s[:m.start()]
+        res += decoded
+        s = s[m.end():]
+
+    return res + s
 
 @app.route('/comments')
 def comments():
     with ScopedSession() as session:
         query = session.query(Comment, User, Post).filter(Comment.user_id == User.user_id).filter(Comment.post_id == Post.post_id)
 
-        before = flask.request.args.get('before')
-        if before is not None:
-            query = query.filter(Comment.posted < parse_date(before))
+        id = flask.request.args.get('id')
+        if id is not None:
+            query = query.filter(Comment.comment_id == id)
+        else:
+            before = flask.request.args.get('before')
+            if before is not None:
+                query = query.filter(Comment.posted < parse_date(before))
 
-        ignore = flask.request.args.get('ignore')
-        if ignore:
-            ignore = [int(u) for u in ignore.split(',')]
-            query = query.filter(Comment.user_id.notin_(ignore))
+            ignore = flask.request.args.get('ignore')
+            if ignore:
+                ignore = [int(u) for u in ignore.split(',')]
+                query = query.filter(Comment.user_id.notin_(ignore))
 
         comments = []
 
@@ -45,7 +70,7 @@ def comments():
                 "id": comment.comment_id,
                 "parent_id": comment.parent_id,
                 "post_id": comment.post_id,
-                "text": comment.text,
+                "text": normalize_text(comment.text),
                 "posted": comment.posted.strftime(DATE_FORMAT),
                 "user_id": user.user_id,
                 "user_name": user.name,
@@ -55,6 +80,7 @@ def comments():
 
     resp = app.make_response(json.dumps(comments, ensure_ascii=False))
     resp.mimetype = 'application/json; charset=utf-8'
+    resp.headers['Access-Control-Allow-Origin'] = '*'
 
     return resp
 
@@ -69,7 +95,7 @@ def post(post_id):
         resp = {
             "id": post.post_id,
             "code": post.code,
-            "text": post.text,
+            "text": normalize_text(post.text),
             "posted": post.posted.strftime(DATE_FORMAT),
             "user_id": user.user_id,
             "user_name": user.name,
@@ -78,19 +104,73 @@ def post(post_id):
         }
 
         comments = []
-        for comment, user in session.query(Comment, User).filter(Comment.post_id == post_id).filter(Comment.user_id == User.user_id).order_by(Comment.posted.asc()).all():
-            comments.append({
-                "id": comment.comment_id,
-                "parent_id": comment.parent_id,
-                "text": comment.text,
-                "posted": comment.posted.strftime(DATE_FORMAT),
-                "user_id": user.user_id,
-                "user_name": user.name,
-                "user_avatar": user.avatar_hash
-            })
+
+        no_comments = flask.request.args.get('no_comments')
+        if not no_comments:
+            for comment, user in session.query(Comment, User).filter(Comment.post_id == post_id).filter(Comment.user_id == User.user_id).order_by(Comment.posted.asc()).all():
+                comments.append({
+                    "id": comment.comment_id,
+                    "parent_id": comment.parent_id,
+                    "text": normalize_text(comment.text),
+                    "posted": comment.posted.strftime(DATE_FORMAT),
+                    "user_id": user.user_id,
+                    "user_name": user.name,
+                    "user_avatar": user.avatar_hash
+                })
         resp["comments"] = comments
 
         resp = app.make_response(json.dumps(resp, ensure_ascii=False))
         resp.mimetype = 'application/json; charset=utf-8'
+        resp.headers['Access-Control-Allow-Origin'] = '*'
 
         return resp
+
+@app.route('/search')
+def search():
+    with ScopedSession() as session:
+        q = flask.request.args.get('query')
+
+        query = session.query(Comment, User, Post, func.ts_headline('russian', Comment.text, func.plainto_tsquery('russian', q), 'HighlightAll=true').label('highlighted')).filter(Comment.user_id == User.user_id).filter(Comment.post_id == Post.post_id)
+
+        query = query.filter(func.to_tsvector('russian', Comment.text).op('@@')(func.plainto_tsquery('russian', q)))
+
+        comments = []
+
+        for comment, user, post, highlighted in query.order_by(func.ts_rank_cd(func.to_tsvector('russian', Comment.text), func.plainto_tsquery('russian', q)).desc(), Comment.posted.desc()).limit(100).all():
+            comments.append({
+                "id": comment.comment_id,
+                "parent_id": comment.parent_id,
+                "post_id": comment.post_id,
+                "text": normalize_text(highlighted),
+                "posted": comment.posted.strftime(DATE_FORMAT),
+                "user_id": user.user_id,
+                "user_name": user.name,
+                "user_avatar": user.avatar_hash,
+                "comment_list_id": post.comment_list_id
+            })
+
+    resp = app.make_response(json.dumps(comments, ensure_ascii=False))
+    resp.mimetype = 'application/json; charset=utf-8'
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+
+    return resp
+
+@app.route('/users')
+def users():
+    with ScopedSession() as session:
+        query = session.query(User)
+
+        users = []
+
+        for user in query.all():
+            users.append({
+                "user_id": user.user_id,
+                "user_name": user.name,
+                "user_avatar": user.avatar_hash
+            })
+
+    resp = app.make_response(json.dumps(users, ensure_ascii=False))
+    resp.mimetype = 'application/json; charset=utf-8'
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+
+    return resp
