ea32504615f883ac9d38ede2c24dce78e894528d
gost-gk <sakuva12+gh_gost@gmail.com>
2020-04-27 00:44:41 +0200

    Fix example systemd units; fix compress_dumps.sh

--- compress_dumps.sh
+++ compress_dumps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOG_FILE="compress.log"
+LOG_FILE="$(realpath compress.log)"
 today_folder="$(date +%Y-%m-%d)/"
 folders_count=0
 
@@ -9,8 +9,8 @@ echo "Compression started at $(date '+%Y-%m-%d %H:%M:%S')..." >> $LOG_FILE
 pushd dumps >/dev/null
 for d in */ ; do
     if [ $d != $today_folder ]; then
-        tar -zcf "${d:0:-1}.tar.gz" $d
-        rm -rf $d
+        tar -zcf "${d:0:-1}.tar.gz" $d >>$LOG_FILE 2>&1
+        rm -rf $d >>$LOG_FILE 2>&1
         ((folders_count=folders_count+1))
     fi
 done
